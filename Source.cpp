7de3d3cb9192da0b816632c4e3aaa199006d092e
Ryan Syme <33377621+NayMyers@users.noreply.github.com>
2018-02-22 12:58:23 +0000

    Add files via upload
    With comments

--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,20 @@
+//Combination lock code to be written in the LMC emulator
 #include <iostream>
 
 using namespace std;
 
 int main(void)
 {
-	int num1, num2, num3;
-	int choice1, choice2, choice3;
-	int check1, check2, check3;
+	int num1, num2, num3; //The three ints that will make up the code 
+	int choice1, choice2, choice3; //three ints that will store guesses 
+	int check1, check2, check3; // three ints that will store the information about whether each particular guess is correct
+	//this will be done by subtracting the guess from the code and storing it in these variables.
+	//if it is zero it is correct and I can use the BRZ (branch zero command) to jump to the next check
 	int change = 1;
 	int open = 0;
-	while (true)
+	while (true) // it is in an infinite loop because this is simulating a combination lock. 
 	{
-		if (change == 1)
+		if (change == 1) //if the user has chosen to change the code at the end it will run this part.
 		{
 			cout << "Enter the first digit of your code	--> ";
 			cin >> num1;
@@ -22,7 +25,7 @@ int main(void)
 			cout << endl;
 			change == 0;
 		}
-		while (open != 1)
+		while (open != 1) //this needs to run if the door is still closed. once it is open in might need to run the change code part.
 		{
 			cout << "Enter the first digit of your guess --> ";
 			cin >> choice1;
@@ -34,7 +37,9 @@ int main(void)
 			check1 = num1 - choice1;
 			check2 = num2 - choice2;
 			check3 = num3 - choice3;
-			if (check1 == 0 && check2 == 0 && check3 == 0)
+			if (check1 == 0 && check2 == 0 && check3 == 0) // In the LMC language all of these will have to be checked seperately and I will use 
+				//the branch zero command to branch to the next check otherwise it will hit the branch allways and branch to outputing 0 to indicate the door is still closed
+				//and then hit the branch allways and return to the top.
 			{
 				cout << "IT OPENS!" << endl;
 				open = 1;
